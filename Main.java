c4b2e0f23710f7eed1d6d9a47b2ba6ff8329543b
VVZ 339 <vvzev@yandex.ru>
2023-05-02 10:18:32 +0300

    bonus

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int x=24_000;//цена билета
+        int m=20;//стоимость одной бонусной мили при покупке билета
+        int b=x/m;//количество бонусных миль
+        System.out.println(b);
+    }
+}
